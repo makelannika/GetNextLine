1492902d39ddfc059463a46612ee7a82e59269f5
Annika Makela <amakela@c1r3p2.hive.fi>
2023-12-22 19:35:51 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,22 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 12:32:38 by amakela           #+#    #+#             */
-/*   Updated: 2023/12/21 12:47:05 by amakela          ###   ########.fr       */
+/*   Updated: 2023/12/21 19:23:50 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdio.h>
 #include "get_next_line.h"
 
+static ssize_t  read_file(int fd, char **text_read);
+static char		*get_line(char **text_read, ssize_t bytes_read);
+
 char	*get_next_line(int fd)
 {
-	size_t		i;
-	char		*temp;
-	char		*line;
-	size_t		bytes_read;
-	static char	*text_read;
-	char		buffer[BUFFER_SIZE + 1];
-
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
+	ssize_t		bytes_read;
+	static char *text_read;
+	char        buffer[BUFFER_SIZE + 1];
+	
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
 		return (NULL);
 	if (!text_read)
 	{
@@ -31,36 +29,58 @@ char	*get_next_line(int fd)
 		if (!text_read)
 			return (NULL);
 	}
-	while ((!ft_strchr(text_read, '\n')) && (bytes_read = read(fd, buffer, BUFFER_SIZE)))
+	bytes_read = read_file(fd, &text_read);
+	if (bytes_read == -1)
+		return (NULL);
+	return (get_line(&text_read, bytes_read));
+}
+
+static ssize_t	read_file(int fd, char **text_read)
+{
+	char	*temp;
+	ssize_t	bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+
+	while ((!ft_strchr(*text_read, '\n')) && (bytes_read = read(fd, buffer, BUFFER_SIZE)))
 	{
 		buffer[bytes_read] = '\0';
-		temp = text_read;
-		text_read = ft_strjoin(text_read, buffer);
+		temp = *text_read;
+		*text_read = ft_strjoin(*text_read, buffer);
 		free(temp);
-		if (!text_read)
-			return (NULL);
+		if (!*text_read)
+			return(-1);
 	}
-	if (bytes_read < 0)
+	if (bytes_read == -1)
 	{
-		free(text_read);
-		return (NULL);
+		free(*text_read);
+		return (-1);
 	}
-	while (text_read[i] && text_read[i] != '\n')
+	return(bytes_read);
+}
+
+static char	*get_line(char **text_read, ssize_t bytes_read)
+{
+	int		i;
+	char	*line;
+	char	*temp;
+
+	i = 0;
+	while ((*text_read)[i] != '\0' && (*text_read)[i] != '\n')
 		i++;
-	if (text_read[i] == '\n')
+	if ((*text_read)[i] == '\n')
 		i++;
-	line = ft_substr(text_read, 0, i);
+	line = ft_substr(*text_read, 0, i);
 	if (!line || bytes_read == 0)
 	{
-		free(text_read);
-		if (!ft_strlen(line))
+		free(*text_read);
+		if (!line)
 			return (NULL);
 		return (line);
 	}
-	temp = text_read;
-	text_read = ft_substr(text_read, i, ft_strlen(text_read) - i);
+	temp = *text_read;
+	*text_read = ft_substr(*text_read, i, ft_strlen(*text_read) - i);
 	free(temp);
-	if (!text_read)
+	if (!*text_read)
 		return (NULL);
 	return (line);
 }
@@ -102,16 +122,16 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)&s[i]);
 	return (0);
 }
-/*
-int main()
-{
-	int	fd;
-	char	*line;
 
-	fd = open("file.txt", O_RDONLY);
-	while ((line = get_next_line(fd)))
-	{
-		printf("%s", line);
-	}
-	return (0);
-}*/
+// int main()
+// {
+// 	int	fd;
+// 	char	*line;
+
+// 	fd = open("file.txt", O_RDONLY);
+// 	while ((line = get_next_line(fd)))
+// 	{
+// 		printf("%s", line);
+// 	}
+// 	return (0);
+// }
